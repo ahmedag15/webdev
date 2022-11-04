bb4aa57f07d46fd9c3eb89c7ba2fb44d7bf4949a
ahmedag15 <ahmed.aghafoor15@gmail.com>
2022-11-05 00:44:04 +0500

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import bgImg from '../assets/mainIMG.jpg';
+import { useForm } from 'react-hook-form';
+
+export default function Form() {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data);
+
+    // console.log(watch('username'));
+    
+  return (
+    <section>
+        <div className="register">
+            <div className="col-1">
+                <h2>Sign In</h2>
+                <span>register and enjoy the service</span>
+
+                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                    <input type="text" {...register("username")} placeholder='username' />
+                    <input type="text" {...register("password")} placeholder='password' />
+                    <button className='btn'>Login</button>
+                </form>
+
+            </div>
+            <div className="col-2">
+                <img src={bgImg} alt="" />
+            </div>
+        </div>
+    </section>
+  )
+}
