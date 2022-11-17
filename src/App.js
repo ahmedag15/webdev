53c83ef2d09198834b488757d01f6438d398bb63
ahmedag15 <ahmed.aghafoor15@gmail.com>
2022-11-18 03:30:06 +0500

    @12

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form';
+import Login from './components/Login';
 import Sidebar from './components/Sidebar';
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from './components/Dashboard';
 
 
@@ -10,11 +10,14 @@ import Dashboard from './components/Dashboard';
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route exact path="/" element={<Form />} />
-        <Route path="/sidebar" element={<Sidebar />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+   
+       <Router>
+        <Routes>
+         <Route exact path="/" element={<Login />} />
+         <Route path="/sidebar" element={<Sidebar />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+       </Router> 
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
+import {AuthProvider} from './context/AuthProvider';
 
 
 
 ReactDOM.render(
-
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <AuthProvider> 
+     <App />
+    </AuthProvider> 
+  </React.StrictMode>,  
+  
   document.getElementById('root')
 
 );
