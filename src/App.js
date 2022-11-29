24eb65d541f6611d605186f8a0071ad427980b70
ahmedag15 <ahmed.aghafoor15@gmail.com>
2022-11-29 22:43:49 +0500

    @14

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './components/Login';
-import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
-
-
+import Departments from './components/Departments';
+import Assets from './components/Assets';
+import EmployeeRecords from './components/EmployeeRecords';
+ 
 function App() {
-
-  var isLoggedin = false;
 
   return (
     <div className='App'>
 
-      {isLoggedin ? <Sidebar /> : <></>}
-
       <Router>
         <Routes>
+
           <Route exact path="/" element={<Login />} />
-          {/* <Route path="/sidebar" element={ } /> */}
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/departments" element={<Departments />} />
+          <Route path="/assets" element={<Assets />} />
+          <Route path="/employeerecords" element={<EmployeeRecords />} />
+        
+
         </Routes>
       </Router>
 
