b03cefc7930c0a2cef157f9699f8c57a2915f06a
ahmedag15 <ahmed.aghafoor15@gmail.com>
2022-11-30 02:08:34 +0500

    @15

---
+++ src/components/Departments.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+
+export default function Departments() {
+
+
+    return (
+        <Sidebar>
+            <div >
+            </div>
+        </Sidebar>
+
+    )
+}
